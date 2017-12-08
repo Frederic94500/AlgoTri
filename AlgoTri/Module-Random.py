25a677286455c08afa5e936c95ed93b7541cb3d6
Frédéric TRAN <frederictran@hotmail.fr>
2017-12-08 22:06:29 +0100

    Ecriture du module random -> fail

--- AlgoTri/Module-Random.py
+++ AlgoTri/Module-Random.py
@@ -1,3 +1,14 @@
-from random import *
-for random in range (0,21):
-    print("Le chiffre no°",random,"est:",randint(0,100))
+from pylab import *
+import matplotlib.pyplot as plt
+import numpy as np
+#from random import *
+#for random in range (1,21):
+    #print("Le chiffre no°",random,"est:",randint(0,100))
+    #random = [randint(0,100)]
+
+#x = array(['liste'])
+#y = array([1, 2, 3, 4])
+random = np.random.randn(100)
+plt.hist(random)
+plt.range(x.min(1), x.max(20))
+plt.show()
