48af0f3529d1a1966c483cf1a247eecdbb4b3458
Frédéric TRAN <frederictran@hotmail.fr>
2017-12-08 21:24:00 +0100

    Mise en commentaire Hello world et ajout du fichier Module-Random

--- AlgoTri/AlgoTri.py
+++ AlgoTri/AlgoTri.py
@@ -1,3 +1,3 @@
 
-print("Hello world!")
+#print("Hello world!")
 #Ceci est un commentaire
---
+++ AlgoTri/Module-Random.py
@@ -0,0 +1,3 @@
+from random import *
+for random in range (0,21):
+    print("Le chiffre no°",random,"est:",randint(0,100))
